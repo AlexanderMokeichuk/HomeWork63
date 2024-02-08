4f0b40a1238744d69a2c2ea86e83ef8ea7f1798c
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-02-08 13:45:26 +0600

    Initial commit!!

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {useState} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
